ce779f051bae242d8954155247355bcc0e7d5d45
Soumyadeep Ghosh <76256823+SoumyadeepGhosh24@users.noreply.github.com>
2023-04-14 08:58:03 +0530

    Update colordetection.py

--- colordetection.py
+++ colordetection.py
@@ -3,11 +3,10 @@ import pandas as pd
 
 
 #Reading the image with opencv
-img_path = r'D:\Computer vision project\image_color_test.jpg'
-img = cv2.imread(img_path)
+image = cv2.imread('D:\Computer vision project\image_color_test.jpg')
 
 #declaring global variables (are used later on)
-clicked = False
+mouse_clicked = False
 r = g = b = xpos = ypos = 0
 
 #Reading csv file with pandas and giving names to each column
@@ -21,17 +20,17 @@ def getColorName(R,G,B):
         d = abs(R- int(csv.loc[i,"R"])) + abs(G- int(csv.loc[i,"G"]))+ abs(B- int(csv.loc[i,"B"]))
         if(d<=minimum):
             minimum = d
-            cname = csv.loc[i,"color_name"]
-    return cname
+            colorname = csv.loc[i,"color_name"]
+    return colorname
 
 #function to get x,y coordinates of mouse double click
 def draw_function(event, x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDBLCLK:
-        global b,g,r,xpos,ypos, clicked
-        clicked = True
+        global b,g,r,xpos,ypos, mouse_clicked
+        mouse_clicked = True
         xpos = x
         ypos = y
-        b,g,r = img[y,x]
+        b,g,r = image[y,x]
         b = int(b)
         g = int(g)
         r = int(r)
@@ -41,24 +40,25 @@ cv2.setMouseCallback('image',draw_function)
 
 while(1):
 
-    cv2.imshow("image",img)
-    if (clicked):
+    cv2.imshow("image",image)
+    if (mouse_clicked):
    
-        #cv2.rectangle(image, startpoint, endpoint, color, thickness)-1 fills entire rectangle 
-        cv2.rectangle(img,(10,10), (550,60), (b,g,r), -1)
+        #cv2.rectangle(image, startpoint, endpoint, color, thickness), 
+        # thickness = -1 fills entire rectangle and if thickness = 1 then background will be transparent
+        cv2.rectangle(image,(10,10), (600,60), (b,g,r), -1)
 
         #Creating text string to display( Color name and RGB values )
         text = getColorName(r,g,b) + ' R='+ str(r) +  ' G='+ str(g) +  ' B='+ str(b)
         
         #cv2.putText(img,text,start,font(0-7),fontScale,color,thickness,lineType )
-        cv2.putText(img, text,(25,50),2,0.8,(255,255,255),2,cv2.LINE_AA)
+        cv2.putText(image, text,(25,50),2,0.8,(255,255,255),2,cv2.LINE_AA)
 
         #For very light colours we will display text in black colour
         if(r+g+b>=600):
             #cv2.putText(img,text,start,font(0-7),fontScale,color,thickness,lineType )
-            cv2.putText(img, text,(50,50),2,0.8,(0,0,0),2,cv2.LINE_AA)
+            cv2.putText(image, text,(50,50),2,0.8,(0,0,0),2,cv2.LINE_AA)
             
-        clicked=False
+        mouse_clicked=False
 
     #Break the loop when user hits 'esc' key    
     if cv2.waitKey(20) & 0xFF ==27:
